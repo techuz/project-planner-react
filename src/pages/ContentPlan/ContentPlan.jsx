054e8e1b96995f5d7ba6c1ecded1b7acdeb8ed66
sanjay-techuz <sanjay@techuz.com>
2023-03-01 18:28:29 +0530

    add content page

---
+++ src/pages/ContentPlan/ContentPlan.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ContentPlanList from '../../components/Table/ContentPlan';
+
+const ContentPlan = () => {
+  return <ContentPlanList />;
+};
+
+export default ContentPlan;
