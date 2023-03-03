c1810128222dd2ab522069276d3ea67af841dd12
Ketan Parmar <ketan.parmar@hauper.com>
2023-03-03 16:57:04 +0530

    Resolved conflict

--- src/App.js
+++ src/App.js
@@ -1,115 +1,118 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import { ProtectedRoute } from './components/ProtectedRoute';
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
-import { Login } from './pages/Login';
-import { Dashboard } from './pages/Projects/Dashboard';
-import { EmployeeList } from './pages/Employee/EmployeeList';
-import { AuthProvider } from './providers/AuthProvider';
-import { ProjectDetail } from './pages/Projects/ProjectDetail';
-import { ProjectExperimentDetail } from './pages/ProjectExperiment/ProjectExperimentDetail';
-import { ContentPlanDetail } from './pages/ContentPlan/ContentPlanDetail';
-import { EmployeeDetail } from './pages/Employee/EmployeeDetail';
-import CreateProject from './components/Form/Project/CreateProject';
-import CreateEmployee from './components/Form/Employee/CreateEmployee';
-import { GlobalDailyStandups } from './pages/GlobalDailyStandups';
-import { ContentPlan } from './pages/ContentPlan/ContentPlanList';
-import { ProjectExperimentList } from './pages/ProjectExperiment/ProjectExperimentList';
+import { Login } from "./pages/Login";
+import { Dashboard } from "./pages/Projects/Dashboard";
+import { EmployeeList } from "./pages/Employee/EmployeeList";
+import { AuthProvider } from "./providers/AuthProvider";
+import { FeatureFlagsProvider } from "./providers/FeatureFlagsProvider";
+import { ProjectDetail } from "./pages/Projects/ProjectDetail";
+import { ProjectExperimentDetail } from "./pages/ProjectExperiment/ProjectExperimentDetail";
+import { ContentPlanDetail } from "./pages/ContentPlan/ContentPlanDetail";
+import { EmployeeDetail } from "./pages/Employee/EmployeeDetail";
+import CreateProject from "./components/Form/Project/CreateProject";
+import CreateEmployee from "./components/Form/Employee/CreateEmployee";
+import { GlobalDailyStandups } from "./pages/GlobalDailyStandups";
+import { ContentPlan } from "./pages/ContentPlan/ContentPlanList";
+import { ProjectExperimentList } from "./pages/ProjectExperiment/ProjectExperimentList";
 
 function App() {
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/employee-list"
-          element={
-            <ProtectedRoute>
-              <EmployeeList />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/global-daily-standups"
-          element={
-            <ProtectedRoute>
-              <GlobalDailyStandups />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/content-plan"
-          element={
-            <ProtectedRoute>
-              <ContentPlan />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/project-experiment"
-          element={
-            <ProtectedRoute>
-              <ProjectExperimentList />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/project-detail/:id"
-          element={
-            <ProtectedRoute>
-              <ProjectDetail />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/employee-detail/:id"
-          element={
-            <ProtectedRoute>
-              <EmployeeDetail />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/project-experiment-detail/:id"
-          element={
-            <ProtectedRoute>
-              <ProjectExperimentDetail />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/content-plan-detail/:id"
-          element={
-            <ProtectedRoute>
-              <ContentPlanDetail />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard/create-new-project"
-          element={
-            <ProtectedRoute>
-              <CreateProject />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/employee-list/create-new-employee"
-          element={
-            <ProtectedRoute>
-              <CreateEmployee />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <FeatureFlagsProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/employee-list"
+            element={
+              <ProtectedRoute>
+                <EmployeeList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/global-daily-standups"
+            element={
+              <ProtectedRoute>
+                <GlobalDailyStandups />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/content-plan"
+            element={
+              <ProtectedRoute>
+                <ContentPlan />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/project-experiment"
+            element={
+              <ProtectedRoute>
+                <ProjectExperimentList />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/project-detail/:id"
+            element={
+              <ProtectedRoute>
+                <ProjectDetail />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/employee-detail/:id"
+            element={
+              <ProtectedRoute>
+                <EmployeeDetail />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/project-experiment-detail/:id"
+            element={
+              <ProtectedRoute>
+                <ProjectExperimentDetail />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/content-plan-detail/:id"
+            element={
+              <ProtectedRoute>
+                <ContentPlanDetail />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard/create-new-project"
+            element={
+              <ProtectedRoute>
+                <CreateProject />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/employee-list/create-new-employee"
+            element={
+              <ProtectedRoute>
+                <CreateEmployee />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </FeatureFlagsProvider>
     </AuthProvider>
   );
 }
--- src/pages/Projects/Dashboard/Dashboard.jsx
+++ src/pages/Projects/Dashboard/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ProjectListTable from '../../../components/Table/Project/List';
+import { useFeatureFlags } from "../../../providers/FeatureFlagsProvider";
 
 const Dashboard = () => {
-  return <ProjectListTable />;
+  const { getIsFeatureEnabled } = useFeatureFlags();
+  const isEnabled = getIsFeatureEnabled('DASHBOARD');
+  //Below code added just for reference, it'll be always true for now
+  return isEnabled ? <ProjectListTable /> : 'Permission Not Granted';
 };
 
 export default Dashboard;
